6fe9082f93790b53b88cec50a8fa300feebc369c
Oscar Baracos <oscar.baracos@gmail.com>
2019-11-27 14:05:00 -0500

    Add vga adapter from lab7

--- not_not.v
+++ not_not.v
@@ -4,6 +4,14 @@ module not_not(
     input CLOCK_50,
     output [6:0] HEX0, HEX1, HEX4, HEX5,
     output [3:0] LEDR
+    output VGA_CLK;   	//	VGA Clock
+	output VGA_HS;		//	VGA H_SYNC
+	output VGA_VS;		//	VGA V_SYNC
+	output VGA_BLANK_N;	//	VGA BLANK
+	output VGA_SYNC_N;	//	VGA SYNC
+	output [9:0] VGA_R; //	VGA Red[9:0]
+	output [9:0] VGA_G; //	VGA Green[9:0]
+	output [9:0] VGA_B; //	VGA Blue[9:0]
 );
     wire [2:0] not_not_selector, color_logic_selector, color_selector_1, color_selector_2;
     wire [3:0] color_1, color2;
@@ -15,6 +23,37 @@ module not_not(
     assign enable = SW[9];
 
     assign LEDR[3:0] = not_not_output;
+	
+	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	wire [2:0] colour;
+	wire [7:0] x;
+	wire [6:0] y;
+	wire writeEn;
+
+    // Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(reset),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+    defparam VGA.RESOLUTION = "160x120";
+    defparam VGA.MONOCHROME = "FALSE";
+    defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+    defparam VGA.BACKGROUND_IMAGE = "black.mif";
+
 
     lfsr_3bits l0(
         .clock(CLOCK_50),
